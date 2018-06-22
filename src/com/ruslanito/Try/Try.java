f5d0cdf3efc153b9ba25c24dc44f908ff5006531
Ruslanito <breitovo76@gmail.com>
2018-06-22 23:24:54 +0300

    First

---
+++ src/com/ruslanito/Try/Try.java
@@ -0,0 +1,9 @@
+package com.ruslanito.Try;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+public class Try {
+    public static void main(String[] args) {
+        System.out.println("*******888********");
+    }
+}

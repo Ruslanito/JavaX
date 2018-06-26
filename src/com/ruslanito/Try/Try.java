8512b573c85a6c5ee582fe8b94984feae49dce44
Ruslanito <breitovo76@gmail.com>
2018-06-26 12:48:56 +0300

    test commit

--- src/com/ruslanito/Try/Try.java
+++ src/com/ruslanito/Try/Try.java
@@ -5,5 +5,7 @@ import com.sun.xml.internal.bind.v2.TODO;
 public class Try {
     public static void main(String[] args) {
         System.out.println("*******888********");
+        System.out.println("++++++++++++++++");
+        System.out.println("XXXXXXXXXXXXXXXXXXX");
     }
 }

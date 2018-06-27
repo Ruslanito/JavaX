435a8ceb12f4f342f361bc3cf6c89a284385f4f1
Ruslanito <breitovo76@gmail.com>
2018-06-27 12:31:32 +0300

    add ArraysListCreatePrint

---
+++ src/com/ruslanito/Core/Core_Arrays/ArraysListCreatePrint.java
@@ -0,0 +1,27 @@
+package com.ruslanito.Core.Core_Arrays;
+
+import java.util.ArrayList;
+
+/*
+Что за список такой?
+*/
+
+public class ArraysListCreatePrint {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<String>();
+        String s = null;
+        int ls = 0;
+        //3
+        for (int i = 0; i < 5; i++) {
+            list.add(s+i);
+        }
+        //4
+
+        System.out.println(list.size());
+
+        //5
+        for (String aList : list) {
+            System.out.println(aList);
+        }
+    }
+}

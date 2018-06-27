6d11eb339bc7ed4145d8c525960d9317fc64fd95
Ruslanito <breitovo76@gmail.com>
2018-06-27 12:39:16 +0300

    add Col_ShortestString

---
+++ src/com/ruslanito/Core/Core_Collection/Col_ShortestString.java
@@ -0,0 +1,33 @@
+package com.ruslanito.Core.Core_Collection;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Выражаемся покороче
+*/
+
+public class Col_ShortestString {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            list.add(rd.readLine());
+        }
+
+        int min = list.get(0).length();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() < min) {
+                min = list.get(i).length();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (min == list.get(i).length()) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}

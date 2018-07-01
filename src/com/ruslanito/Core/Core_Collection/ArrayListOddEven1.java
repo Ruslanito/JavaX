6fcdde5a9ff6da6d10cdeba3bebc139a032d3e1a
Ruslanito <breitovo76@gmail.com>
2018-07-01 15:48:01 +0300

    add ArrayListOddEven2 and rename ArrayListOddEven

--- src/com/ruslanito/Core/Core_Collection/ArrayListOddEven1.java
+++ src/com/ruslanito/Core/Core_Collection/ArrayListOddEven1.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class ArrayListOddEven {
+public class ArrayListOddEven1 {
 
     public static void main(String[] args) throws IOException
     {
---
+++ src/com/ruslanito/Core/Core_Collection/ArrayListOddEven2.java
@@ -0,0 +1,32 @@
+package com.ruslanito.Core.Core_Collection;
+
+// Разделение массива на два — чётных и нечётных чисел
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ArrayListOddEven2 {
+    public static void main(String[] args) throws IOException
+    {
+        //статическая инициализация массива
+        int[] data = {1, 5, 6, 11, 3, 15, 7, 8};
+
+        //создание списка, где все элементы должны быть типа Integer
+        ArrayList<Integer> list = new ArrayList<Integer> ();
+
+        //заполнение списка из массива
+        for (int i = 0; i < data.length; i++) list.add(data[i]);
+
+        ArrayList<Integer> even = new ArrayList<Integer>();  //чётные
+        ArrayList<Integer> odd = new ArrayList<Integer>();    //нечётные
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            Integer x = list.get(i);
+            if (x % 2 == 0)    //если x - чётное
+                even.add(x);   // добавляем x в коллекцию четных чисел
+            else
+                odd.add(x);    // добавляем x в коллекцию нечетных чисел
+        }
+    }
+}

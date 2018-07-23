792f5f0fedae4bdebac9d94676156ca90059b0bc
Ruslanito <breitovo76@gmail.com>
2018-07-23 23:05:52 +0300

    create Swing_CreateConstructor

---
+++ src/com/ruslanito/Core/Core_Swing/Swing_CreateConstructor.java
@@ -0,0 +1,22 @@
+package com.ruslanito.Core.Core_Swing;
+
+import javax.swing.*;
+
+public class Swing_CreateConstructor {
+    JFrame fr;
+
+    public static void main(String[] args) {
+        new Swing_CreateConstructor();
+    }
+
+    Swing_CreateConstructor(){
+        fr = new JFrame();
+        int a=300;
+        int b=300;
+        fr.setSize(a,b);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setTitle("Frame Constructor");
+        fr.setVisible(true);
+        fr.setLayout(null);
+    }
+}

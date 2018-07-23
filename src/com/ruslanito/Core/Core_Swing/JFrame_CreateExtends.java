ddb1d20db22849176cf07c2a42428c3653c300a7
Ruslanito <breitovo76@gmail.com>
2018-07-23 22:18:34 +0300

    rename to JFrame_CreateExtends

--- src/com/ruslanito/Core/Core_Swing/JFrame_CreateExtends.java
+++ src/com/ruslanito/Core/Core_Swing/JFrame_CreateExtends.java
@@ -5,12 +5,12 @@ import javax.swing.*;
 /**
  * Created by Ruslanito on 25.02.2017.
  */
-public class JFrame_JFrame extends JFrame{
+public class JFrame_CreateExtends extends JFrame{
     public static void main(String[] args) {
-        JFrame_JFrame f = new JFrame_JFrame();
+        JFrame_CreateExtends f = new JFrame_CreateExtends();
     }
         // создаём конструктор с параметрами окна
-    public JFrame_JFrame(){
+    public JFrame_CreateExtends(){
         int a=300;
         int b=300;
         setSize(a,b);
--- src/com/ruslanito/Core/Core_Swing/JFrame_Window.java
+++ src/com/ruslanito/Core/Core_Swing/JFrame_Window.java
@@ -5,24 +5,24 @@ public class JFrame_Window {
 		public static void main(String[] args) {
 		int a,b;
 		String c, d;
-				c = JOptionPane.showInputDialog("¬ведите число a");
-				d = JOptionPane.showInputDialog("¬ведите число b");
+				c = JOptionPane.showInputDialog("Введите число a");
+				d = JOptionPane.showInputDialog("Введите число b");
 						a = Integer.parseInt(c);
 						b = Integer.parseInt(d);
 		int min;
 		if (a<b){
 			min=a;
-			JOptionPane.showMessageDialog(null, "¬ведЄное число a меньше b:"+a);
+			JOptionPane.showMessageDialog(null, "Введеное число a меньше b:"+a);
 			//System.out.println("¬ведЄное число a меньше b:"+a);
 		}
 		if (a>b){
 			min=b;
-			JOptionPane.showMessageDialog(null, "¬ведЄное число b меньше a:"+b);
+			JOptionPane.showMessageDialog(null, "Введеное число b меньше a:"+b);
 			//System.out.println("¬ведЄное число b меньше a:"+b);
 		}
 		if(a==b){
 			min=a=b;
-			JOptionPane.showMessageDialog(null, "„исла равны");
+			JOptionPane.showMessageDialog(null, "Числа равны");
 			//System.out.println("¬ведЄные числа a и b  равны");
 		}
 	}

13f36c6e140e6ac12fc720c31af276c81ae7ceb2
Ruslanito <breitovo76@gmail.com>
2018-07-23 22:37:54 +0300

    rename Swing_CreateExtends

--- src/com/ruslanito/Core/Core_Swing/Swing_CreateExtends.java
+++ src/com/ruslanito/Core/Core_Swing/Swing_CreateExtends.java
@@ -5,13 +5,13 @@ import javax.swing.*;
 /**
  * Created by Ruslanito on 25.02.2017.
  */
-public class JFrame_CreateExtends extends JFrame{
+public class Swing_CreateExtends extends JFrame{
     public static void main(String[] args) {
 
-        JFrame_CreateExtends f = new JFrame_CreateExtends();
+        Swing_CreateExtends f = new Swing_CreateExtends();
     }
         // создаём конструктор с параметрами окна
-    public JFrame_CreateExtends(){
+    public Swing_CreateExtends(){
         int a=300;
         int b=300;
         setSize(a,b);

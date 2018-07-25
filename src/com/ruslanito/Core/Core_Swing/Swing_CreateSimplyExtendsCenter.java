7698979db79f860a8884b50b79d10aa22ff2550f
Ruslanito <breitovo76@gmail.com>
2018-07-26 00:09:56 +0300

    add Swing_CreateSimplyExtendsCenter

---
+++ src/com/ruslanito/Core/Core_Swing/Swing_CreateSimplyExtendsCenter.java
@@ -0,0 +1,27 @@
+package com.ruslanito.Core.Core_Swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Swing_CreateSimplyExtendsCenter {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JFrameExtand"){};
+
+//        frame.setSize(500,500);//dimensions of frame
+//        frame.setLocation(150,300);//start points of frame's left top corner
+        frame.setVisible(true);//using no layout
+        frame.setLayout(null);//making the frame visible
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit on close
+//        frame.setBounds(150,300,500,500);//replace setSize & setLocation
+
+//   use resolution of monitor
+        Toolkit toolkit = Toolkit.getDefaultToolkit();// 1-st stage to get resolution of monitor
+        Dimension dimension = toolkit.getScreenSize();// 2-nd stage to get resolution of monitor
+        frame.setBounds(dimension.width/2-250,dimension.height/2-250,500,500);//replace setSize & setLocation
+
+
+
+    }
+
+}

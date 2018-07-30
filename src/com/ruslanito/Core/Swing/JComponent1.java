38dac32406fe7d0538510a63eb17b3750b2e5302
Ruslanito <breitovo76@gmail.com>
2018-07-30 12:15:43 +0300

    work on JComponent1 - 2

--- src/com/ruslanito/Core/Swing/JComponent1.java
+++ src/com/ruslanito/Core/Swing/JComponent1.java
@@ -2,7 +2,10 @@ package com.ruslanito.Core.Swing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 /*C:\Users\Ruslan\IdeaProjects\JavaX\src\com\ruslanito\Core\Swing\SwingCreate\Swing_CreateMethod.java*/
 
@@ -34,6 +37,28 @@ public class JComponent1 {
 //add line in window
             Line2D l2 = new Line2D.Double(70, 70, 190, 190);//coordinates of line
             g2.draw(l2);
+
+//line through points
+            Point2D p1 = new Point2D.Double(80,80);
+            Point2D p2 = new Point2D.Double(200,200);
+            Line2D l3 = new Line2D.Double(p1, p2);
+            g2.draw(l3);
+
+//ellipse
+            Ellipse2D el = new Ellipse2D.Double(100,100,150,200);
+//            g2.draw(el); //draw ellipse
+//            g2.setPaint(new Color(12,12,255));//add color for filling
+            g2.setPaint(Color.ORANGE);// 2-nd variant for add color for filling
+            g2.fill(el);//draw ellipse with fill it
+
+//rectangle
+            Rectangle2D rec = new Rectangle2D.Double(250,120,150,40);
+            g2.setPaint(Color.CYAN);
+            g2.draw(rec);
+
+//image
+
+
 
         }
     }

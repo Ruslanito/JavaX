de1aa809ee79b3907d3c92773a0dbf47c0d84e43
Ruslanito <breitovo76@gmail.com>
2018-07-30 09:52:12 +0300

    work on JComponent1

--- src/com/ruslanito/Core/Swing/JComponent1.java
+++ src/com/ruslanito/Core/Swing/JComponent1.java
@@ -2,6 +2,7 @@ package com.ruslanito.Core.Swing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 /*C:\Users\Ruslan\IdeaProjects\JavaX\src\com\ruslanito\Core\Swing\SwingCreate\Swing_CreateMethod.java*/
 
@@ -23,21 +24,27 @@ public class JComponent1 {
 
     static class MyComponent extends JComponent {
         @Override
-        protected void paintComponent(Graphics g){
-            Graphics2D g2 = (Graphics2D)g;
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+//add text in window
             Font font = new Font("Arial", Font.BOLD, 20);
             g2.setFont(font);
-            g2.drawString("Hello World", 20,20);
+            g2.drawString("Hello World!", 200, 20);// font and start point (from left top corner)
+
+//add line in window
+            Line2D l2 = new Line2D.Double(70, 70, 190, 190);//coordinates of line
+            g2.draw(l2);
+
         }
     }
 
-    static JFrame getFrame(){
+    static JFrame getFrame() {
         JFrame jframe = new JFrame("Window");
         jframe.setVisible(true);
         jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
-        jframe.setBounds(dimension.width/2-250, dimension.height/2-250, 500,500);
+        jframe.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 250, 500, 500);
         return jframe;
     }
 }
